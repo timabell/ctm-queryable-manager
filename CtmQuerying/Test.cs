a3be06177877a7c840ee88a4e1611c93ac4fb2bc
Tim Abell <tim@timwise.co.uk>
2014-03-03 23:17:25 +0000

    interesting broken case (RED)
    Shows that where clause means max isn't satisfied correctly

--- CtmQuerying/Test.cs
+++ CtmQuerying/Test.cs
@@ -1,15 +1,46 @@
 using System;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CtmQuerying
 {
 	[TestFixture()]
 	public class Test
 	{
+		List<Widget> mockData = new List<Widget>{
+			new Widget{ Id=1},
+			new Widget{ Id=2},
+			new Widget{ Id=3},
+		};
+
 		[Test()]
-		public void TestCase ()
+		public void TestMax ()
 		{
+			var manager = new Manager {Max=2, repo=mockData.AsQueryable()};
+			var results = manager.GetStuff().ToList();
+			Assert.AreEqual(2, results.Count);
 		}
+
+		[Test()]
+		public void TestFilter ()
+		{
+			var manager = new Manager {Max=2, repo=mockData.AsQueryable()};
+			var results = manager.GetStuff().Where(widget => widget.Id !=2).ToList();
+			Assert.AreEqual(2, results.Count);
+		}
+	}
+
+	class Manager{
+		public int Max { get; set; }
+		public IQueryable<Widget> repo { get; set; }
+		public IQueryable<Widget> GetStuff(){
+			return repo.Take(Max);
+		}
+	}
+
+	class Widget{
+		public int Id { get; set; }
 	}
 }
 
